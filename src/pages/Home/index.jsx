642e3e0cafb91d790f3a0924a32e9f74de2fce47
Yan Santos <yansantos.dev@gmail.com>
2022-11-08 16:27:56 -0300

    estilizando page home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,30 @@
+import './home.css'
 export default function Home(){
 
     return(
       
-        <h1>
-            home
-        </h1>
-      
+     <div className='home-container'>
+        <h1>Leleco Dev</h1>
+        <span>Veja meus links 🤝</span>
+
+        <main className='links'>
+            <section className='link-area'>
+                <a href="#">
+                    <p className='link-text'>Canal no youtube</p>
+                </a>
+            </section>
+            <section className='link-area'>
+                <a href="#">
+                    <p className='link-text'>Canal no youtube</p>
+                </a>
+            </section>
+            <section className='link-area'>
+                <a href="#">
+                    <p className='link-text'>Canal no youtube</p>
+                </a>
+            </section>
+        </main>
+       
+     </div>
     )
 }
